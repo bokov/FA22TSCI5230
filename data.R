63e6d2b207ca66cc117d4282b6eec20f461ef18d
Xingyu <xingyu@g1200164494.v806.med.uthscsa.edu>
2023-09-20 17:00:44 -0500

    09/20/23

--- data.R
+++ data.R
@@ -36,6 +36,7 @@ library(fs);    # file system operations
 
 options(max.print=42);
 panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
+# download data
 if(!file.exists('data.R.rdata')){
   #' # Import the data
   Input_Data <- 'https://physionet.org/static/published-projects/mimic-iv-demo/mimic-iv-clinical-database-demo-1.0.zip';
@@ -54,4 +55,28 @@ if(!file.exists('data.R.rdata')){
 
 load(file = "data.R.rdata")
 
+democolumns = c('subject_id','insurance', 'marital_status', 'ethnicity')
 
+length_unique = function(x) unique(x) %>% length()
+
+unique_values = function(x) unique(x) %>% sort() %>% paste(sep = '_',collapse = ':')
+
+# vectorize running it by col
+sapply(admissions[,democolumns], function(x) x %>% unique() %>% length())
+
+
+admissions %>% group_by(subject_id) %>%  summarise(across(any_of(democolumns), length_unique))
+#" # Demongraphic table
+
+demographics = admissions %>% group_by(subject_id) %>%
+  summarise(across(any_of(democolumns), unique_values)
+            ,decease = any(!is.na(deathtime))
+            ,deathtime = max(deathtime, na.rm = T)
+              ) %>%
+  mutate(ethnicity_revised = str_replace(ethnicity, 'UNKNOWN;', ''),
+         ethnicity_revised_gsub = gsub('UNKNOWN;', '', ethnicity),
+         )
+
+demographics[is.infinite(demographics$deathtime), "deathtime"] = NA
+
+demographics %>% left_join(patients[,c("subject_id", "gender","anchor_age")])
